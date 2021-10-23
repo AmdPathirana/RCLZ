b3532141d90111fc4b1573e139f73ef01429369c
AmdPathirana <2018is052@stu.ucsc.cmb.ac.lk>
2021-10-23 19:54:55 +0530

    reactInstalled

---
+++ day2/es6.js
@@ -0,0 +1,21 @@
+let num1 = 5;
+let myNAME;
+myNAME="AMOD";
+let n=3;
+n=4;
+console.log(n);
+
+const name="amod";
+console.log(`My name is ${name} Pathirana.` ) // es6 wala new cnocatanation backticks athule use kranna spacing wala hondai. 
+
+function printName(){
+    const nameTwo="amd";
+    console.log(`       ma 
+    nn ${nameTwo}`);
+}
+printName();
+
+function add(num1, num2){
+    return num1+num2;
+}
+console.log(add(2,3));
