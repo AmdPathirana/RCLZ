c0c4e1f65fc0c9f0be6f8348ccc31328b32297c2
AmdPathirana <2018is052@stu.ucsc.cmb.ac.lk>
2021-10-23 21:09:48 +0530

    Update4

--- day3/my-app/src/App.js
+++ day3/my-app/src/App.js
@@ -1,10 +1,17 @@
 
 import './App.css';
+import Box from './components/Box';
+import './App.css';
 
 function App() {
   return (
-    <div>
-        <h1>Hello!</h1>
+    <div className="container">
+        <Box name="box1"/>
+        <Box name="box2"/>
+        <Box name="box3"/>
+        <Box name="box4"/>
+        <Box name="box5"/>
+        <Box name="box6"/>
     </div>
   );
 }
